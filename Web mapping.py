56a059ae531e40101046091c6cc71da6ce8a6210
rhaff <hafezuraaheeman@gmail.com>
2024-02-09 11:31:42 +0530

    Added python file

--- Web mapping.py
+++ Web mapping.py
@@ -1,3 +1,5 @@
 import folium
-map=folium.Map(location=[13.02, 80.22], zoom_start=15)
-map.save('Map.html')
+
+map1 = folium.Map(location=[13.02, 80.22], zoom_start=15, tiles="CartoDB Positron",
+                  attr='Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.')
+map1.save('Map.html')
